d133d949fb4a234e98ad25731639c96602ad290f
Michal-Grzegorzek <93224108+Michal-Grzegorzek@users.noreply.github.com>
2022-12-26 13:06:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ login_manager.init_app(app)
 
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///cafes.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_", "sqlite:///cafes.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
